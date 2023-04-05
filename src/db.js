8a7712bf799d8d8704ecfb30a2820d9cd6b0d4a5
greckb <luar.28@gmail.com>
2023-04-05 19:30:07 +0200

    new config

--- src/db.js
+++ src/db.js
@@ -1,13 +1,14 @@
-import {createPool} from 'mysql2/promise';
+import { createPool } from 'mysql2/promise';
 import dotenv from 'dotenv';
 dotenv.config();
 
-export const pool  = createPool({
-    host: process.env.MYQLHOST,
-    user: process.env.MYSQLUSER,
-    password: process.env.MYSQLPASSWORD,
-    port: process.env.MYSQLPORT,
-    database: process.env.MYSQLDATABASE,
-    })
+export const pool = createPool({
+  host: process.env.MYSQLHOST,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  port: process.env.MYSQLPORT,
+  database: process.env.MYSQLDATABASE,
+});
+
     
     
