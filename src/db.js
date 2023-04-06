26258b0a1a0d9729fcc48dbe751292d944632550
greckb <luar.28@gmail.com>
2023-04-06 16:04:54 +0200

    AWS pruebas

--- src/db.js
+++ src/db.js
@@ -1,16 +1,11 @@
-import { createPool } from 'mysql2/promise';
-import dotenv from 'dotenv';
-dotenv.config();
+import {createPool} from 'mysql2/promise';
 
-export const pool = createPool({
-  host: '178.211.133.14',
-  user: 'zftuyhpu_juanjo',
-  password: 'Surf&Roll1',
-  port: 3306,
-  database: 'zftuyhpu_juanjo',
-});
-
-
-
+export const pool  = createPool({
+    host: '178.211.133.14',
+    user: 'zftuyhpu_juanjo',
+    password: 'Surf&Roll1',
+    port: 3306,
+    database: 'zftuyhpu_gimnasio',
+    })
     
     
--- src/server.js
+++ src/server.js
@@ -1,3 +1,4 @@
+
 import express from 'express'
 import cors from 'cors'
 import bodyParser from 'body-parser';
@@ -6,12 +7,6 @@ import clientes from './routes/clientes.routes.js'
 import datos from './routes/datos.routes.js'
 import login from './routes/login.routes.js'
 import remesas from './routes/remesas.routes.js'
-import correo from './routes/correo.routes.js'
-import dotenv from 'dotenv';
-dotenv.config();
-import dns from 'dns';
-import os from 'os';
-
 
 
 const app = express();
@@ -31,47 +26,24 @@ app.use(
 
 (async function() {
   try {
-    const connection = await pool.getConnection();
-    const result = await connection.query('SELECT * FROM ADMINISTRADORES');
-  
-    // console.log(`Connected to database ${JSON.stringify(result[0])}`);
-    connection.release();
+    await pool.getConnection();
+    console.log('Connected to database');
   } catch (error) {
     console.error('Error connecting to database:', error);
   }
 })();
 
-
-
 //Traer todas las funciones de Clientes
 app.use(clientes)
-
-app.get('/', (req,res)=>{
-  dns.lookup(os.hostname(), function (err, address, family) {
-    console.log(`La dirección IP del host es ${address}`);
-  });
-  res.send('Welcome to my Apy!')
-})
 
 //Traer el arbol del programa
 app.use(datos)
 
 app.use(remesas)
 
+
+
 app.use(login)
 
-app.use(correo)
-
-// function worker() {
-//   setInterval(() => {
-//     console.log('werk');
-//   }, 1000);
-// }
-
-// throng({
-//   workers: 1,
-//   start: worker
-// });
-
 const port = process.env.PORT || 8080;
-app.listen(port, '0.0.0.0', () => console.log(`Listening on port ${port}`));
+app.listen(port, () => console.log(`Listening on port ${port}..`));
