323f6879841a2d7728030daa8d5f99ea49f74ba9
greckb <luar.28@gmail.com>
2023-04-12 19:36:19 +0200

    cambios

--- src/server.js
+++ src/server.js
@@ -18,7 +18,7 @@ app.use(
   cors({
     allowedHeaders: ["authorization", "Content-Type"], // you can change the headers
     exposedHeaders: ["authorization"], // you can change the headers
-    origin: "*",
+    origin: "https://prueba-esi.vercel.app/",
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
     preflightContinue: false
   })
