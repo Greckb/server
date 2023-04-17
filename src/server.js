09f396a3b7abaaebda58620aff7daceefd993f91
greckb <luar.28@gmail.com>
2023-04-17 06:25:28 +0200

    commit

--- src/server.js
+++ src/server.js
@@ -69,6 +69,8 @@ app.use(login);
 
 app.use(correo);
 
+app.get('/',(req,res)=> res.send('hello'))
+
 const port = process.env.PORT || 8080;
 app.listen(port, () => console.log(`Listening on port ${port}..`));
 
