59ee47a9182f7a2ee950c0b61fe281248a57a8f6
greckb <luar.28@gmail.com>
2023-04-17 20:00:16 +0200

    puerto

--- src/server.js
+++ src/server.js
@@ -71,7 +71,7 @@ app.use(correo);
 
 app.get('/',(req,res)=> res.send('hello'))
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 app.listen(port, () => console.log(`Listening on port ${port}..`));
 
 // Opciones para el servidor HTTPS
