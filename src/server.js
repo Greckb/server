d4e1025471b2b2e0a5096c440f5a59e6897eb79b
greckb <luar.28@gmail.com>
2023-09-07 19:57:30 +0200

    fix

--- src/server.js
+++ src/server.js
@@ -32,7 +32,7 @@ const corsOptions = {
 app.use(cors(corsOptions));
 
 // Configurar middleware para archivos estáticos
-app.use(express.static(path.join(__dirname, 'src/public')));
+app.use(express.static(path.join(__dirname, 'server/src/public')));
 
 (async function () {
   try {
