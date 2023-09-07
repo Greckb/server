392f004e30085b28879abc54c93da328b4d4cd71
greckb <luar.28@gmail.com>
2023-09-07 19:37:41 +0200

    git

--- src/db.js
+++ src/db.js
@@ -5,8 +5,5 @@ dotenv.config(); // Cargar variables de entorno desde el archivo .env
 
 export const pool = createPool({
     host: '178.211.133.14',
-    user: 'zftuyhpu_juanjo',
-    password: 'Surf&Roll1',
-    port: 3306,
-    database: 'zftuyhpu_gimnasio',
+   
 });
