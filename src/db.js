bdb74fd03304c55430393b044924eed0a03b7a05
greckb <luar.28@gmail.com>
2023-09-07 19:37:00 +0200

    fix

--- src/db.js
+++ src/db.js
@@ -4,5 +4,9 @@ import dotenv from 'dotenv';
 dotenv.config(); // Cargar variables de entorno desde el archivo .env
 
 export const pool = createPool({
-
+    host: '178.211.133.14',
+    user: 'zftuyhpu_juanjo',
+    password: 'Surf&Roll1',
+    port: 3306,
+    database: 'zftuyhpu_gimnasio',
 });
